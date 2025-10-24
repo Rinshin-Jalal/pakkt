7e898f38cb872674affbd4848b3fe3ead83c31eb
Rinshin Jalal <rinzhinjalal@gmail.com>
2025-10-24 16:20:20 +0530

    build: add Supabase, KeychainAccess, and Kingfisher dependencies

--- app/Package.swift
+++ app/Package.swift
@@ -1,15 +1,34 @@
 // swift-tools-version: 6.2
-// The swift-tools-version declares the minimum version of Swift required to build this package.
-
 import PackageDescription
 
 let package = Package(
     name: "Pakkt",
+    platforms: [
+        .iOS(.v16)
+    ],
+    products: [
+        .library(
+            name: "Pakkt",
+            targets: ["Pakkt"]
+        )
+    ],
+    dependencies: [
+        .package(url: "https://github.com/supabase/supabase-swift.git", from: "2.0.0"),
+        .package(url: "https://github.com/kishikawakatsumi/KeychainAccess.git", from: "4.2.2"),
+        .package(url: "https://github.com/onevcat/Kingfisher.git", from: "7.10.0")
+    ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .executableTarget(
-            name: "Pakkt"
+        .target(
+            name: "Pakkt",
+            dependencies: [
+                .product(name: "Supabase", package: "supabase-swift"),
+                .product(name: "KeychainAccess", package: "KeychainAccess"),
+                .product(name: "Kingfisher", package: "Kingfisher")
+            ]
         ),
+        .testTarget(
+            name: "PakktTests",
+            dependencies: ["Pakkt"]
+        )
     ]
 )
